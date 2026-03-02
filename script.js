fc3b007bc0ac1283f1182d77028b915da2cedbf9
Edilson Paulo Chivinge <edilsonchivinge499@gmail.com>
2026-03-02 21:23:41 +0200

    Labs aplicados.

---
+++ script.js
@@ -0,0 +1,65 @@
+        // LAB 3: Contador
+        let numeroLikes = 0;
+        
+        document.getElementById("btn-curtir").addEventListener("click", function() {
+            numeroLikes++; // Aumenta 1
+            document.getElementById("contador").innerText = numeroLikes; // Atualiza a tela
+        });
+
+        // LAB 4: Login
+        document.getElementById("btn-login").addEventListener("click", function() {
+            let emailDigitado = document.getElementById("email").value;
+            let senhaDigitada = document.getElementById("senha").value;
+            let paragrafoMensagem = document.getElementById("mensagem-login");
+
+            if (emailDigitado === "admin" && senhaDigitada === "1234") {
+                paragrafoMensagem.innerText = "Login feito";
+                paragrafoMensagem.className = "sucesso";
+            } else {
+                paragrafoMensagem.innerText = "Credenciais inválidas";
+                paragrafoMensagem.className = "erro";
+            }
+        });
+
+        
+        // LAB 5: Todo List (Com botão de remover)
+      
+        document.getElementById("btn-adicionar").addEventListener("click", function() {
+            let input = document.getElementById("input-tarefa");
+            let textoDaTarefa = input.value;
+
+            if (textoDaTarefa !== "") {
+                let novoItem = document.createElement("li");
+                novoItem.innerText = textoDaTarefa + " ";
+
+                let botaoRemover = document.createElement("button");
+                botaoRemover.innerText = "Remover";
+                
+                botaoRemover.addEventListener("click", function() {
+                    novoItem.remove(); 
+                });
+
+                novoItem.appendChild(botaoRemover);
+                document.getElementById("lista-tarefas").appendChild(novoItem);
+
+                input.value = "";
+            }
+        });
+
+        //
+        // MINI PROJETO: Sistema de Inscriçã
+        document.getElementById("btn-inscrever").addEventListener("click", function() {
+        
+            let idadeDigitada = document.getElementById("idade").value;
+            let mensagemInscricao = document.getElementById("mensagem-inscricao");
+
+            
+            if (idadeDigitada < 18) {
+                mensagemInscricao.innerText = "Erro: É necessário ter 18 anos ou mais.";
+                mensagemInscricao.className = "erro";
+            } else {
+                mensagemInscricao.innerText = "Inscrição feita com sucesso!";
+                mensagemInscricao.className = "sucesso";
+            }
+        });
+    
